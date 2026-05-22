135e4137d9b15740926c3e93080e3e778c12b246
Badar Ahmad <badarbhatt.pk786@gmail.com>
2026-05-22 10:03:00 +0500

    Delete tools/generate-vapid.php

--- tools/generate-vapid.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '../vendor-push/autoload.php';
-use Minishlink\WebPush\VAPID;
-
-$keys = VAPID::createVapidKeys();
-echo "Public Key: " . $keys['publicKey'] . "<br>";
-echo "Private Key: " . $keys['privateKey'] . "<br>";
-?>
